7c48fd71b2f949c6f9fdefc9428606e03aba0fa9
Raja Sekar Manimaran <raja.sekar.manimaran@automattic.com>
2026-01-03 18:01:07 +0530

    add env config to the app using viper package

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -7,19 +7,19 @@ import (
 	"testing"
 
 	_ "github.com/lib/pq"
+	"github.com/raja651/simplebank/util"
 )
 
 var testQueries *Queries
 var testDb *sql.DB
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:1234@localhost:5432/simple_bank?sslmode=disable"
-)
-
 func TestMain(m *testing.M) {
 	var err error
-	testDb, err = sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+	testDb, err = sql.Open(config.DBDriver, config.DBSource)
 
 	if err != nil {
 		log.Fatal("cannot connect to db: ", err)
